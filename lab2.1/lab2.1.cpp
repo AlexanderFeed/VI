aed6521298d0c4c0e3c12651bbd69ad773a8db7c
Александр Николаевич Мищенко <alex1mishchenko@gmail.com>
2022-12-01 00:12:53 +0300

    Lr2

--- lab2.1/lab2.1.cpp
+++ lab2.1/lab2.1.cpp
@@ -1,20 +1,95 @@
-﻿// lab2.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
 
-#include <iostream>
+vector <int> readFile(int& k)
+{
+
+	string strN, s;
+	vector <int> m = {};
+	ifstream file("input.txt");
+	if (file.is_open())
+	{
+		file >> k;
+		int b;
+		//m = new int[k];
+		for (int i = 0; i < k; i++)
+		{
+			file >> b;
+			m.push_back(b);
+		}
+	}
+	return m;
+}
+
+bool haveNextBool(vector <bool> s)
+{
+	int index = s.size() - 1;
+	while (index != 0 && s[index] == true)
+		if (s[index] == true)
+			index--;
+	if (s[index] == true)
+		return false;
+	return true;
+}
+
+vector <bool> nextBool(vector < bool> s)
+{
+	int n = s.size() - 1;
+	while (s[n] == true)
+		n--;
+	s[n] = true;
+	for (int i = n + 1; i < s.size(); i++)
+		s[i] = false;
+	return s;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	vector <int> m;
+	int k = 0;
+	m = readFile(k);
+	cout << "k = " << k << endl;
+	for (int i = 0; i < k; i++)
+	{
+		cout << m[i] << endl;
+	}
+	vector <bool> s;
+	vector <int> save;
+	vector <vector <int>> res;
+	for (int i = 0; i < k; i++)
+		s.push_back(false);
+	int sum = 0;
+
+	while (haveNextBool(s))
+	{
+		s = nextBool(s);
+		for (int j = 0; j < k; j++)
+			cout << s[j] << "\t";
+		cout << endl;
+		sum = 0;
+		save.clear();
+		for (int j = 0; j < k; j++)
+			if (s[j] == true) {
+				sum += m[j];
+				save.push_back(m[j]);
+			}
+		if (sum % k == 0)
+			res.push_back(save);
+	}
+	ofstream output("output.txt");
+	if (res.size() != 0)
+	{
+		output << "Yes\n";
+		for (int i = 0; i < res.size(); i++)
+		{
+			for (int j = 0; j < res[i].size(); j++)
+				output << res[i][j] << "\t";
+
+			output << "\n";
+		}
+	}
+	else output << "No";
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
